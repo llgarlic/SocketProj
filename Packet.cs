c563741a984bfa2e463aa99530c44abe0ad1aac8
llgarlic <73431572+llgarlic@users.noreply.github.com>
2021-06-24 13:30:57 +0700

    Upload codes needed to run
    Need to create projects on visual studio, in image.png. 
    ServerData is a class library

---
+++ Packet.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServerData;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Net;
+
+namespace ServerData
+{
+
+
+    [Serializable]
+    public class Packet{
+        public List<string> Gdata;
+        public int packetInt;
+        public bool packetBool;
+        public string senderID;
+        public PacketType packetType;
+
+        public Packet(PacketType type,string senderID)
+        {
+            Gdata = new List<string>();
+            this.senderID = senderID;
+            this.packetType = type;
+        }
+
+        public Packet(byte[] packetbytes)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream(packetbytes);
+
+            Packet p = (Packet)bf.Deserialize(ms);
+            ms.Close();
+            this.Gdata = p.Gdata;
+            this.packetInt = p.packetInt;
+            this.packetBool = p.packetBool;
+            this.senderID = p.senderID;
+            this.packetType = p.packetType;
+        }
+
+        public byte[] toBytes()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream();
+
+            bf.Serialize(ms, this);
+            byte[] bytes = ms.ToArray();
+            ms.Close();
+            return bytes;
+        }
+
+
+        public static string GetIPAddress()
+        {
+            IPAddress[] ips = Dns.GetHostAddresses(Dns.GetHostName());
+
+            foreach(IPAddress i in ips)
+            {
+                if (i.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork) //neu no dang connect
+                    return i.ToString();
+            }//tra ip cua client
+            return "127.0.0.1"; //neu k thi tra ip default
+        }
+
+    }
+
+    public enum PacketType
+    {
+        Registration,
+        Chat
+    }
+}
---
+++ Server.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using ServerData;
+using System.Net;
+
+namespace Server
+{
+    class Server
+    {
+        static Socket listenerSocket; //dung de nghe
+        static List<ClientData> _clients;
+
+        static void Main(string[] args){
+            Console.WriteLine("starting server on " + Packet.GetIPAddress());
+
+            listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _clients = new List<ClientData>();
+
+            IPEndPoint ip = new IPEndPoint(IPAddress.Parse(Packet.GetIPAddress()), 4242);
+            //lay ip cua may
+
+            listenerSocket.Bind(ip); //cai ip cua may hien tai
+            
+            Thread listenThread = new Thread(ListenThread); //tao thread
+            listenThread.Start();
+
+        } //khoi tao server
+
+
+        static void ListenThread()
+        {
+            for(; ; )
+            {
+                listenerSocket.Listen(0);
+                _clients.Add(new ClientData(listenerSocket.Accept())); //them socket vao trong list
+            }
+
+        }//listener - nhan biet cac client dang connect
+
+        public static void Data_IN(object cSocket)
+        {
+            Socket clientSocket = (Socket)cSocket;
+
+            byte[] buffer;
+            int readbytes;
+
+            for(; ; )
+            {
+                try
+                {
+                    buffer = new byte[clientSocket.SendBufferSize];
+
+                    readbytes = clientSocket.Receive(buffer);
+
+                    if (readbytes > 0)
+                    {
+                        //xu li data o day
+                        Packet packet = new Packet(buffer);
+                        DataManager(packet);
+                    }
+                }
+                catch(SocketException ex)
+                {
+                    Console.WriteLine("a client disconnected! ");
+                    Console.ReadLine();
+                }
+            }
+        }//clientdata thread - nhan data tu tung client
+
+
+        public static void DataManager(Packet p)
+        {
+            switch (p.packetType)
+            {
+                case PacketType.Chat:
+                    foreach (ClientData c in _clients)
+                        c.clientSocket.Send(p.toBytes());
+                    break;
+            }
+        }
+
+        //quan ly data
+    }
+
+    class ClientData
+    {
+        public Socket clientSocket;
+        public Thread clientThread; //
+        public string id;
+
+        public ClientData() {
+            id = Guid.NewGuid().ToString();
+            clientThread = new Thread(Server.Data_IN);
+            clientThread.Start(clientSocket);
+            SendRegistrationPacket();
+        }
+        public ClientData(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+            id = Guid.NewGuid().ToString();
+            clientThread = new Thread(Server.Data_IN);
+            clientThread.Start(clientSocket);
+            SendRegistrationPacket();
+
+        }
+
+        public void SendRegistrationPacket()
+        {
+            Packet p = new Packet(PacketType.Registration, "server");
+            p.Gdata.Add(id);
+            clientSocket.Send(p.toBytes());
+        }
+
+    }
+
+}
